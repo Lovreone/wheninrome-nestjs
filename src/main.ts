128cd26c911e098de807bec69d3be4668f68f22f
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-04-29 14:15:52 +0200

    Added console info on app start

--- src/main.ts
+++ src/main.ts
@@ -23,5 +23,7 @@ async function bootstrap() {
   SwaggerModule.setup('/api-docs', app, document); 
 
   await app.listen(3000);
+
+  console.info(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
