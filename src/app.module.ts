06815a77c70b9b23fcfef9b3994cc1488cd6a081
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-04-29 14:46:03 +0200

    Added Auth and User modules/services skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module'
 import { LandmarksModule } from './landmarks/landmarks.module';
 import { CitiesModule } from './cities/cities.module';
+;
 
 @Module({
   imports: [
     MongooseModule.forRoot(Config.mongoConn),
+    AuthModule,
+    UsersModule,
     CitiesModule,
     LandmarksModule
   ],
