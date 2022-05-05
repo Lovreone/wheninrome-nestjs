5e4ae33b2c28ba70744ba8b7eda1cf913f469ffa
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-05 17:27:09 +0200

    Users: Added a basic pre-save hook

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,7 +5,24 @@ import { UsersService } from './users.service';
 import { UserSchema, User } from './user.model';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    // MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeatureAsync(
+      [ // Docs: "Hooks" https://docs.nestjs.com/techniques/mongodb
+        { 
+          name: User.name,
+          useFactory: () => {
+            const schema = UserSchema;
+            schema.pre('save', function () {
+                // TODO: this['password'] = this['password'] + ':saltsaltsalt';
+                console.log('Hello from pre save', this);
+            });
+            return schema;
+          }
+        } 
+      ]
+    )
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
