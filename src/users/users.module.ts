c425505f264702c9ef62b7b0f965b78350c74475
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-06 14:35:26 +0200

    Users - Removing pre-save hook, taking different appoach due to maintainability

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,23 +5,8 @@ import { UsersService } from './users.service';
 import { UserSchema, User } from './user.model';
 
 @Module({
-  imports: [
-    // MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    MongooseModule.forFeatureAsync(
-      [ // Docs: "Hooks" https://docs.nestjs.com/techniques/mongodb
-        { 
-          name: User.name,
-          useFactory: () => {
-            const schema = UserSchema;
-            schema.pre('save', function () {
-                // TODO: this['password'] = this['password'] + ':saltsaltsalt';
-                console.log('Hello from pre save', this);
-            });
-            return schema;
-          }
-        } 
-      ]
-    )
+  imports: [ 
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]) 
   ],
   controllers: [UsersController],
   providers: [UsersService],
