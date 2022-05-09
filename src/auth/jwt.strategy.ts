5799cb30395ed3aa198c396432de61188778fe4e
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-09 23:15:24 +0200

    Passport strategy comment docs light cleanup

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -7,41 +7,28 @@ import { jwtConstants } from './constants';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
-    /* Configuration options available
-       https://github.com/mikenicholson/passport-jwt#configure-strategy */
+    /* Config opts available https://github.com/mikenicholson/passport-jwt#configure-strategy */
     super({
-      /* jwtFromRequest: supplies the method by which the JWT will be extracted from 
-      the Request. We will use the standard approach of supplying a bearer token in 
-      the Authorization header of our API requests. Other options are described here: 
-      https://github.com/mikenicholson/passport-jwt#extracting-the-jwt-from-the-request */
+      /* jwtFromRequest: supplies the method by which the JWT will be extracted from the Request. 
+      We use the standard approach of supplying a bearer token in the Authorization header of our API requests. 
+      Other options are described here: https://github.com/mikenicholson/passport-jwt#extracting-the-jwt-from-the-request */
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      /* ignoreExpiration: just to be explicit, we choose the default false setting, 
-      which delegates the responsibility of ensuring that a JWT has not expired to 
-      the Passport module. This means that if our route is supplied with an expired JWT, 
-      the request will be denied and a 401 Unauthorized response sent. 
-      Passport conveniently handles this automatically for us. */
+      /* ignoreExpiration: We choose the default false setting, delegating check of JWT expiration to Passport module.
+      If our route is supplied with an expired JWT, request will be denied with 401 Unauthorized response. */
       ignoreExpiration: false,
       /* secretOrKey: we are using the expedient option of supplying a symmetric secret 
-      for signing the token. Other options, such as a PEM-encoded public key, 
-      may be more appropriate for production apps (see here for more information):
-      https://github.com/mikenicholson/passport-jwt#configure-strategy
-      In any case, as cautioned earlier, do not expose this secret publicly. */
+      for signing the token. Do not expose this secret publicly. */
       secretOrKey: jwtConstants.secret,
     });
   }
 
-  /* Recall again that Passport will build a user object based on the return value 
-  of our validate() method, and attach it as a property on the Request object.
-    It's also worth pointing out that this approach leaves us room ('hooks' as it were) 
-    to inject other business logic into the process. 
-    For example, we could do a database lookup in our validate() 
-    method to extract more information about the user, resulting in a more enriched user
-     object being available in our Request. This is also the place we may decide to do 
-     further token validation, such as looking up the userId in a list of revoked tokens, 
-     enabling us to perform token revocation. The model we've implemented here in our sample
-     code is a fast, "stateless JWT" model, where each API call is immediately authorized
-      based on the presence of a valid JWT, and a small bit of information about the requester 
-      (its userId and username) is available in our Request pipeline. */
+  /* Passport builds user object based on validate() method return value, and attachex it as a property on the Request object.
+  This approach leaves us room to inject other business logic into the process, like doing a database lookup in validate() 
+  to extract more user info, returning a more enriched user.
+
+  This is also where we could do further token validation, like looking up userId in a list of revoked tokens, enabling us to 
+  perform token revocation. The model implemented here is a fast, "stateless JWT" model, where each API call is immediately authorized 
+  based on valid JWT presence, and a small bit of info about the requester (its userId and username) is available in our Request pipeline. */
   async validate(payload: any) {
     return { userId: payload.sub, username: payload.username };
   }
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -6,57 +6,29 @@ import { AuthService } from './auth.service';
 // TODO: Clean up all comments when finished
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
+  
+  /* Docs: https://docs.nestjs.com/security/authentication
+  [1] Set of options specific to the strategy. 
+  We configure a Passport strategy by extending the PassportStrategy class. 
+  We pass the strategy options [1] by calling the super() in our subclass, optionally passing it an options object.
+  Strategy configuration options available 
+    http://www.passportjs.org/concepts/authentication/strategies/
+    https://www.passportjs.org/packages/
+    https://github.com/jaredhanson/passport-local */
   constructor(private authService: AuthService) {
+    /** Overriding default local-strategy option (username) with 'email' */
     super({ 
       usernameField: 'email',
       passwordField: 'password'
     });
-    /* https://docs.nestjs.com/security/authentication
-    Vanilla Passport
-    [1] A set of options that are specific to that strategy. 
-    For example, in a JWT strategy, 
-    you might provide a secret to sign tokens.
-
-    With @nestjs/passport, you configure a Passport strategy 
-    by extending the PassportStrategy class. 
-    You pass the strategy options [1] 
-    by calling the super() method in your subclass, 
-    optionally passing in an options object.
-    
-    TODO: 
-    We can pass an options object in the call to super() 
-    to customize the behavior of the passport strategy. 
-    In this example, the passport-local strategy by default 
-    expects properties called username and password in the request body. 
-    Pass an options object to specify different property names, 
-    for example: super({ usernameField: 'email' })
-
-    Check strategy configuration options available 
-        http://www.passportjs.org/concepts/authentication/strategies/
-        https://www.passportjs.org/packages/
-        https://github.com/jaredhanson/passport-local*/
   }
 
-  /* Vanilla Passport
-  [2] A "verify callback", which is where you tell Passport how to 
-  interact with your user store (where you manage user accounts). 
-  Here, you verify whether a user exists (and/or create a new user), 
-  and whether their credentials are valid. T
-  he Passport library expects this callback to return a full user 
-  if the validation succeeds, or a null if it fails 
-  (failure is defined as either the user is not found, or, 
-    in the case of passport-local, the password does not match).
-
-  With @nestjs/passport You provide the verify callback [2] 
-  by implementing a validate() method in your subclass.
+  /* [2] A "verify callback", which is where you tell Passport how to interact with your user store (where you manage user accounts). 
+  Here, you verify whether a user exists, and whether their credentials are valid. 
+  Passport library expects this callback to return a full user if the validation succeeds, or a null if it fails  (user not found or password mismatch).
   
-  For each strategy, Passport will call the verify function 
-  (implemented with the validate() method in @nestjs/passport) 
-  using an appropriate strategy-specific set of parameters. 
-  For the local-strategy, Passport expects a validate() method
-   with the following signature: 
-   validate(username: string, password:string): any.
-  */
+  For each strategy, Passport will call it's validate() method, using strategy-specific set of parameters. 
+  For local-strategy, Passport expects method with signature: validate(username: string, password:string): any. */
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
