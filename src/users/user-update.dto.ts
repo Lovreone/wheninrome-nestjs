9438cc1389c9bb7bccdc137c54098c9c4ade6c4f
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-25 19:22:07 +0200

    UserUpdateDto - Custom validation messages

--- src/users/user-update.dto.ts
+++ src/users/user-update.dto.ts
@@ -2,36 +2,35 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength, MaxLength, IsEmail, IsBoolean } from 'class-validator';
 import { Role } from 'src/helpers/enums';
 
-// FIXME: Rethink what we allow to be changed by admin and what for user
 export class UserUpdateDTO {
     @ApiProperty({description: 'User designated username (Unique)', minimum: 2, maximum: 20})
-    @IsNotEmpty()
+    @IsNotEmpty({message: '\'$property\' field is required.'})
     @IsString()
     @MinLength(2)
     @MaxLength(20)
     username: string;
 
     @ApiProperty({description: 'User designated email (Unique)'})
-    @IsNotEmpty()
-    @IsString()
-    @IsEmail()
+    @IsNotEmpty({message: '\'$property\' field is required.'})
+    @IsEmail({}, {message: '\'$property\' field: invalid email format for \'$value\' value.'})
     email: string;
 
     @ApiProperty({description: 'First name of the User', minimum: 2, maximum: 20})
-    @IsNotEmpty()
+    @IsNotEmpty({message: '\'$property\' field is required.'})
     @IsString()
     @MinLength(2)
     @MaxLength(20)
     firstName: string;
 
     @ApiProperty({description: 'Last name of the User', minimum: 2, maximum: 20})
-    @IsNotEmpty()
+    @IsNotEmpty({message: '\'$property\' field is required.'})
     @IsString()
     @MinLength(2)
     @MaxLength(20)
     lastName: string;
 
     @ApiProperty({description: 'Flag determining whether user account is active or not'})
+    @IsNotEmpty({message: '\'$property\' field is required.'})
     @IsBoolean()
     isActive: boolean;
 
