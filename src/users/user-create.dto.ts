e8f82ee3ff9c2931c72c8af2eadcb6d3b968d32e
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-26 17:36:23 +0200

    UserCreateDto: Added decorator error messages

--- src/users/user-create.dto.ts
+++ src/users/user-create.dto.ts
@@ -4,33 +4,32 @@ import { Role } from 'src/helpers/enums';
 
 export class UserCreateDTO {
     @ApiProperty({description: 'User designated username (Unique)', minimum: 2, maximum: 20})
-    @IsNotEmpty()
+    @IsNotEmpty({message: '\'$property\' field is required.'})
     @IsString()
     @MinLength(2)
     @MaxLength(20)
     username: string;
 
     @ApiProperty({description: 'User designated email (Unique)'})
-    @IsNotEmpty()
-    @IsString()
-    @IsEmail()
+    @IsNotEmpty({message: '\'$property\' field is required.'})
+    @IsEmail({}, {message: '\'$property\' field: invalid email format for \'$value\' value.'})
     email: string;
 
     @ApiProperty({description: 'User designated password', minimum: 8})
-    @IsNotEmpty()
+    @IsNotEmpty({message: '\'$property\' field is required.'})
     @IsString()
     @MinLength(8)
     password: string;
 
     @ApiProperty({description: 'First name of the User', minimum: 2, maximum: 20})
-    @IsNotEmpty()
+    @IsNotEmpty({message: '\'$property\' field is required.'})
     @IsString()
     @MinLength(2)
     @MaxLength(20)
     firstName: string;
 
     @ApiProperty({description: 'Last name of the User', minimum: 2, maximum: 20})
-    @IsNotEmpty()
+    @IsNotEmpty({message: '\'$property\' field is required.'})
     @IsString()
     @MinLength(2)
     @MaxLength(20)
